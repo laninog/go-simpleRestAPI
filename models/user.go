fbb0b8b5a0c4c84bc9fbf08c38e5b7486f83e45b
Alejandro Niño <alejandro6230@gmail.com>
2018-12-11 11:14:27 +0100

    Create domain models

---
+++ models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	ID        string   `json:"id,omitempty"`
+	FirstName string   `json:"firstname,omitempty"`
+	LastName  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
